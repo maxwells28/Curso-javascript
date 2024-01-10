d41b9727ad8bc5ac9733416f6ea0d3c14133fec7
Maxwell da silva moreira <145509371+maxwells28@users.noreply.github.com>
2024-01-10 10:10:19 -0300

    aula js

---
+++ aula011/ex008.js
@@ -0,0 +1,5 @@
+var vel = 75 ;
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if( vel > 60) {console.log(`Voce foi MULTADO por ultrapassar o limite de velocidade`)}
+console.log(`dirija sempre usando cinto de segurança!`)
