5687b897661d3dc8f2b397001459c6bd4a9af73e
Maxwell da silva moreira <145509371+maxwells28@users.noreply.github.com>
2024-01-18 21:12:38 -0300

    exercicio 014

--- aula012exer/exer014/script.js
+++ aula012exer/exer014/script.js
@@ -1,1 +1,22 @@
-alert('ola')
+function carregar(){
+var msg = window.document.getElementById('img')
+window.document.getElementById('foto')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `agora são ${hora} horas`
+}
+if( hora >= 0 && hora < 12){
+    Imag.src = 'manha.png'
+    document.body.style.backgroundColor = '#e2cd9f'
+
+}
+    else if (hora >= 12 && hora < 18){
+        Imag.src = 'tarde.png'
+
+
+    }
+    else{
+        Imag.src = 'noite.png'
+
+
+    }
