4793ea2055b26bffda9a69b02bd5aca01834054e
Maxwell da silva moreira <145509371+maxwells28@users.noreply.github.com>
2024-01-20 18:25:39 -0300

    exercicio 14 javascript concluido

--- aula012exer/exer014/script.js
+++ aula012exer/exer014/script.js
@@ -1,22 +1,30 @@
 function carregar(){
-var msg = window.document.getElementById('img')
-window.document.getElementById('foto')
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
+//var hora = 
 msg.innerHTML = `agora são ${hora} horas`
-}
+
 if( hora >= 0 && hora < 12){
-    Imag.src = 'manha.png'
+    img.src = "fotomanha.png"
     document.body.style.backgroundColor = '#e2cd9f'
+  
+   
 
 }
     else if (hora >= 12 && hora < 18){
-        Imag.src = 'tarde.png'
-
+        img.src = "fototarde.png"
+        document.body.style.backgroundColor = '#b9846f'
+       
+        
 
     }
     else{
-        Imag.src = 'noite.png'
+        img.src = "fotonoite.png"
+        document.body.style.backgroundColor = '#515154'
+        
 
 
     }
+}
