59a97b5dc12966d7c244afe0fbfaf87dff2e9d73
Maxwell da silva moreira <145509371+maxwells28@users.noreply.github.com>
2024-02-15 21:49:11 -0300

    exercicio 017 tabuada

--- aula014exer/exer017/script.js
+++ aula014exer/exer017/script.js
@@ -1,1 +1,25 @@
-alert('ola')
+
+function calcular(){
+    var tabuada = parseInt(document.getElementById('valor').value)
+    var resultado = document.getElementById('res')
+
+    resultado.innerHTML = ''
+    
+    if(!isNaN(tabuada)){   
+    
+       for(var t = 1; t <= 10; t++){
+          var multiplicação = tabuada * t
+          resultado.innerHTML += tabuada + " x " + t + " = " + multiplicação + '<br>'
+        }
+
+    }else{
+        resultado.innerHTML = 'Por favor, difite um numero valido'
+    } 
+
+    
+}
+document.getElementById('botaovalor').addEventListener('click', calcular)
+
+
+
+    
