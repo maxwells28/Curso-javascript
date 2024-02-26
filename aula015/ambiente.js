71eb8fa2f2c868178b87155985f89474cba05fca
Maxwell da silva moreira <145509371+maxwells28@users.noreply.github.com>
2024-02-26 20:13:25 -0300

    continuando funções

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -1,3 +1,6 @@
 let num = [5,8,2,9,3]
-
+num.push(1)
+num.sort()
 console.log(`Nosso vetor é o ${num}`)
+console.log(`O nosso vetor tem ${num.length} posições`)
+console.log(`O primeiro vetor é ${num[0]}`)
