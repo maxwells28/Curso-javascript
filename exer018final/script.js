3eb83ab0ab76c4c5de542352799f86bf59fd9fe6
Maxwell da silva moreira <145509371+maxwells28@users.noreply.github.com>
2024-02-27 19:26:32 -0300

    exercicio final

---
+++ exer018final/script.js
@@ -0,0 +1,6 @@
+function adicionar(add){
+    let adicionar  = document.getElementById('add')
+    addEventListener(adicionar, onclick)
+    let res = document.getElementById('visorn')
+    res.innerHTML = 'visorn'
+}
