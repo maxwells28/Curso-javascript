0cffc301920730a78416bacc651150bdb9f9fb6e
Maxwell da silva moreira <145509371+maxwells28@users.noreply.github.com>
2024-03-14 18:46:55 -0300

    js

--- exer018final/script.js
+++ exer018final/script.js
@@ -1,6 +1,2 @@
-function adicionar(add){
-    let adicionar  = document.getElementById('add')
-    addEventListener(adicionar, onclick)
-    let res = document.getElementById('visorn')
-    res.innerHTML = 'visorn'
-}
+
+
